147af82de1e9ba7e25cd5452da27a25d005ba3be
chengjianglong <cjfykx@gmail.com>
2016-06-30 17:04:07 -0400

    The initial release version of the simulated-kinect.

---
+++ build/CMakeFiles/VisualizeSDFWorld.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VisualizeSDFWorld.dir/VisualizeSDFWorld.cpp.o"
+  "VisualizeSDFWorld.pdb"
+  "VisualizeSDFWorld"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VisualizeSDFWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
